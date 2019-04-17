7e8ffa8fa70075b7b69a201abcb93e96420defb9
Kaitezidis, Orestis <o.kaitezidis@gatech.edu>
2019-04-17 00:17:13 -0400

    Add files via upload

--- src/R_Code/lasso.R
+++ src/R_Code/lasso.R
@@ -1,7 +1,11 @@
+#### LASSO Exploration for Variable Selection #######
+
+# Read Data
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 df<-read.csv("../../data/preprocessed/football.csv",header=TRUE)
 
+# Load Libraries
 set.seed(123)    # seef for reproducibility
 library(car)
 library(glmnet)
@@ -16,6 +20,7 @@ sum(is.na(df))
 Upset <- df$Upset
 df = df[ , !(names(df) %in% c('Upset','HomeTeam','AwayTeam'))]
 
+# Scale features
 ind <- sapply(df, is.numeric)
 df[ind] <- lapply(df[ind], scale)
 df = cbind(df, Upset)
@@ -26,9 +31,13 @@ X <- model.matrix(Upset~.,df)
 lambdas_to_try  <- 10^seq(10, -2, length = 100)
 cv.out <- cv.glmnet(X, y, alpha = 1, lambda = lambdas_to_try,standardize = TRUE, nfolds = 10)
 #plot(cv.out)
+
+# Find best lambda
 bestlam=cv.out$lambda.min
 coef(cv.out)
 lasso_lm <-glmnet(X, y, alpha = 1, lambda = bestlam )
 tmp_coeffs <- coef(lasso_lm)
+
+# Dat frame with coefficients
 data_frame_coeffs = data.frame(name = tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1], coefficient = tmp_coeffs@x)
 
